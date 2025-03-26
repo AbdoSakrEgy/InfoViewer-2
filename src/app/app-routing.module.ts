64ba6031ced51a132dbee9aa9877c08c7b508e54
AbdoSakrEgy <abdulrahimsakr01@gmail.com>
2025-03-26 21:19:26 +0200

    update routes and rebuild for netlify deployment

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,6 +7,11 @@ const routes: Routes = [
   { path: 'InfoViewer-2', component: UsersInfoPageComponent },
   { path: '', redirectTo: 'InfoViewer-2', pathMatch: 'full' },
   { path: '**', component: PageNotFoundComponent },
+  {
+    path: 'abdosakregy.github.io/user-info/users-info',
+    redirectTo: 'InfoViewer-2',
+    pathMatch: 'full',
+  }, //old path
 ];
 
 @NgModule({
